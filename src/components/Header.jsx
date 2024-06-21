f903eb5d00c2a5f84a9a84d679fc85b8f6c6f79c
gorbachyovKS <gorbachyokvKS@gmail.com>
2024-06-21 23:29:27 +0300

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import cl from '../styles/Header.module.css'
 
 const Header = () => {
+	const IMG = useMemo(() => {
+		return require('../assets/image/backgrounds/header_bg.jpg')
+	})
 	return (
-		<header className={cl.header}>
-			<div className={cl.wrapper}>
-				<div className={cl.logo}>
-					<a className={cl.logo__img} href='/'>
-						<img src="./assets/image/logo/word-black.PNG" alt="logo" />
-					</a>
+		<>
+			<header
+				className={cl.header}
+			>	
+				<div
+					className={cl.background}
+					style={{
+						background: `url(${IMG})`,
+						backgroundPosition: 'center center',
+						backgroundRepeat: 'no-repeat',
+						backgroundSize: 'cover'
+					}}
+				>
+
 				</div>
-			</div>
-		</header>
+			</header>
+			<header
+				className={cl.header}
+				style={{
+					background: 'var(--beige)',
+					height: '103px',
+					marginBottom: '297px',
+					position: 'sticky',
+					top: '0',
+					zIndex: '100',
+				}}
+			>
+				<div className={cl.wrapper}>
+					<div className={cl.logo}>
+						<a className={cl.logo__img} href='/'>
+							<img src="./assets/image/logo/word-black.PNG" alt="logo" />
+						</a>
+					</div>
+				</div>
+			</header>
+		</>
 	)
 }
 
